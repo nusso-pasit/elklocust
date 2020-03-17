40c4004a1af189d57d513bafbce75038dfb93b42
Pasit Nusso <nusso.pasit@gmail.com>
2020-03-17 12:53:12 +0700

    update locust

--- locust_scripts/locustfile.py
+++ locust_scripts/locustfile.py
@@ -74,7 +74,7 @@ class WebsiteTasks(TaskSet):
             if response_data.get("data",{}).get("updateMessage",{}) is not None :
                 r.success()
             else:
-                r.failure(response_data.get("errors", {})[0]['message'][:256])
+                r.failure(response_data.get("errors", {})[0]['message'][:255])
     @task(20)
     def webhookMessages(self):
         json ={
